6e3a251b3eb1e307711542f6089d4554be4d54ba
EmbergenovAJ <19ayda967198@gmail.com>
2025-07-03 20:14:55 +0500

    back url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://courseprojectback.onrender.com",
 });
 
 // При наличии токена — подставляем его в заголовки
